86aa766583ebce4faf4ba88ffcce5577b78e77b3
merveBILGIC <58006664+merveBILGIC@users.noreply.github.com>
2020-12-07 20:19:52 +0300

    Add files via upload

---
+++ İKİZ_ASAL_SAYILAR/İKİZ_ASAL_SAYILAR/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace İKİZ_ASAL_SAYILAR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "KONTROL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1.SAYI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2.SAYI";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(343, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(422, 245);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "İKİZ ASAL SAYILAR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ İKİZ_ASAL_SAYILAR/İKİZ_ASAL_SAYILAR/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İKİZ_ASAL_SAYILAR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int kontrol = 1;
+       public  int  asal (int nesne)
+        {
+            
+            switch (nesne)
+            {
+                case int a when a<=0:
+                    {
+                        MessageBox.Show("pozitif ve sıfırdan büyük bir sayı girin!!!");
+                        kontrol = 0;
+                        break;
+                    }
+                case int b when b == 2:
+                    {
+                        kontrol = 1;
+                        break;
+                    }
+                case int c when c <= 2:
+                    {
+                        for(int i=2;i<c/2;i++)
+                        {
+                            if (c % i == 0)
+                                kontrol = 0;
+                            else
+                                kontrol = 1;
+                        }
+                        break;
+                    }
+                    
+            }
+
+            return kontrol;
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nesne;
+            int sayı1, sayı2;
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+            if(sayı1<sayı2)
+            {
+                MessageBox.Show("sayı1 sayı2 den büyük olmalıdır!!!");
+            }
+            asal(sayı1);
+            asal(sayı2);
+            nesne = kontrol;
+            if (nesne == 0) 
+            {
+                MessageBox.Show("sayılar asal degildir ikizlikleri kontrol edilemez!!!!");
+             
+            }
+            if(nesne==1)
+            {
+                if (sayı1 - sayı2 == 2)
+                {
+                    MessageBox.Show("sayılar ikiz asal sayılardır");
+                }
+                else
+                    MessageBox.Show("sayılar ikiz asal sayı değildir!!!");
+            }
+        }
+    }
+}
